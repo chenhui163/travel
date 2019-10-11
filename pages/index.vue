509d468e3946c3bfb5bda8ca4d1d1ef53c47bbb0
chenhui163 <chenhui_github@163.com>
2019-10-11 23:23:29 +0800

    向服务器请求首页轮播图数据，并渲染页面成功

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,59 @@
 <template>
-    <div>
-        首页
+    <div class="container">
+        <!-- 幻灯片 -->
+        <el-carousel 
+        :interval="5000" 
+        arrow="always">
+            <el-carousel-item
+                v-for="(item,index) in banners"
+                :key="index"
+            >
+                <div class="banner-image"
+                    :style="`background:url(${$axios.defaults.baseURL + item.url}) center center no-repeat;
+                    background-size:contain contain;`"
+                >
+                </div>
+            </el-carousel-item>
+        </el-carousel>
     </div>
 </template>
 
 <script>
 export default {
+    // 数据
+    data(){
+        return {
+            // 轮播图数据
+            banners: []
+        }
+    },
 
+    // 页面加载完毕时执行
+    mounted(){
+        // 请求首页轮播图数据
+        this.$axios({
+            url:"/scenics/banners"
+        }).then(res=>{
+            const {data} = res.data;
+            this.banners = data;
+        })
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>

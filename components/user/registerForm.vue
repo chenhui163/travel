6212f6fde3dda298969b9cd6abb156be96997d4e
chenhui163 <chenhui_github@163.com>
2019-10-12 19:23:24 +0800

    注册部分绑定表单数据，进行表单规则验证

--- components/user/registerForm.vue
+++ components/user/registerForm.vue
@@ -4,14 +4,16 @@
         ref="form" 
         :rules="rules" 
         class="form">
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="username">
                 <el-input 
+                v-model="form.username"
                 placeholder="用户名手机">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="captcha">
                 <el-input 
+                v-model="form.captcha"
                 placeholder="验证码" >
                     <template slot="append">
                         <el-button @click="handleSendCaptcha">
@@ -21,21 +23,24 @@
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="nickname">
                 <el-input 
+                v-model="form.nickname"
                 placeholder="你的名字">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
-                <el-input 
+            <el-form-item class="form-item" prop="password">
+                <el-input
+                v-model="form.password" 
                 placeholder="密码" 
                 type="password"
                 ></el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="checkPassword">
                 <el-input 
+                v-model="form.checkPassword"
                 placeholder="确认密码" 
                 type="password">
                 </el-input>
@@ -58,12 +63,31 @@ export default {
         return {
             // 表单数据对象
             form: {
-                
+                username: "",
+                captcha: "",    // 验证码
+                nickname: "",
+                password: "",
+                checkPassword: ""   // 确认密码
             },
             // 表单验证规则
             rules: {
-
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' }
+                ],
+                captcha: [
+                    { required: true, message: '请输入验证码', trigger: 'blur' }
+                ],
+                nickname: [
+                    { required: true, message: '请输入昵称', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' }
+                ],
+                checkPassword: [
+                    { required: true, message: '两次密码不一致', trigger: 'blur' }
+                ]
             }
+            
         }
     },
 
